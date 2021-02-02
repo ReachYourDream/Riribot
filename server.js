5ac6d16a15f8a749cdc2f754dda760836239cb05
ReachYourDream <radityopradana@gmail.com>
2021-02-02 20:02:38 +0700

    adding keep alive for glints

--- server.js
+++ server.js
@@ -2,6 +2,20 @@
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const http = require('http');
+const express = require('express');
+const app = express();
+
+//keep alive glitch method
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
 
 
 
