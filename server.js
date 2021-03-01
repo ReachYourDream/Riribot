2d13b4af720e365b9e1d58939d058afd7cd5d210
ReachYourDream <radityopradana@gmail.com>
2021-03-01 23:57:22 +0700

    add detector for user joining voice chat

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ const DEFAULT_PREFIX = process.env.PREFIX;
 Bluebird.promisifyAll(mongoose);
 
 //keep alive glitch method
-app.get("/", (request, response) => {
-  console.log(Date.now() + " Ping Received");
+app.get('/', (request, response) => {
+  console.log(Date.now() + ' Ping Received');
   response.sendStatus(200);
 });
 app.listen(process.env.PORT);
@@ -35,41 +35,46 @@ client.on('message', async (message) => {
   if (!message.guild) return;
 
   const guildFound = await guildController.findById(message.guild.id);
-  const prefix =  _.get(guildFound, '[0].prefix', DEFAULT_PREFIX);
+  const prefix = _.get(guildFound, '[0].prefix', DEFAULT_PREFIX);
 
-  if(message.content ==="!prefix"){
+  if (message.content === '!prefix') {
     return message.channel.send(`Prefix for this channel is: \`${prefix}\``);
   }
   if (message.content.indexOf(prefix) !== 0) return;
 
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
-  const commandFileObject = _.find(commandList(), {'command': command});
+  const commandFileObject = _.find(commandList(), { command: command });
   const commandFile = commandFileObject ? commandFileObject.fileName : '';
 
   // The list of if/else is replaced with those simple 2 lines:
   try {
     let commandRequired = require(`./commands/${commandFile}`);
-    if(commandRequired.length <= 0){
+    if (commandRequired.length <= 0) {
       const embedError = new Discord.MessageEmbed()
-        .addField("I can't find command ", command)
+        .addField('I can\'t find command ', command)
         .setColor('RED');
       return message.channel.send(embedError);
     }
-    await commandRequired.run(client, message, args)
-      .catch(async err => {
+    await commandRequired.run(client, message, args).catch(async (err) => {
       console.log(err);
       const embedError = new Discord.MessageEmbed()
-          .addField("I can't find command ", command)
-          .setColor('RED');
+        .addField('I can\'t find command ', command)
+        .setColor('RED');
       await message.channel.send(embedError);
     });
   } catch (err) {
     console.log(err);
     const embedError = new Discord.MessageEmbed()
-      .addField("I can't find command ", command)
+      .addField('I can\'t find command ', command)
       .setColor('RED');
     await message.channel.send(embedError);
+  }
+});
+
+client.on('voiceStateUpdate', (oldState, newState) => {
+  if (newState.channelID === 'idChannelX') {
+    newState.setChannel('idChannelY', 'just move 4Head');
   }
 });
 
